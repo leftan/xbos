e427c8129ef00e928d4395dc6e61aa60ae9722f1
Yifei Liu <leftan12121@gmail.com>
2018-01-23 22:04:32 -0800

    initiate the repo

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $( "#hvac" ).click(function() {
+ 
+        if ( $( this ).hasClass( "protected" ) ) {
+     
+            console.log("checked");   
+     
+        }
+ 
+    });
+
+    myfunction function() {
+            
+    });
+    
+
+});
