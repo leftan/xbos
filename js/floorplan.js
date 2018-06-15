2a6b0793a32a6a4c68ffabd529b7aec67ea83f5d
Yifei Liu <yifeiliu@Yifeis-MacBook-Pro-2.local>
2018-06-15 16:19:54 -0700

    clean up a bit

---
+++ js/floorplan.js
@@ -0,0 +1,69 @@
+       $("#zone2").hover(function(){
+            //console.log("hover");
+            $(this).css("color", "rgba(0,0,0,1)");
+            $(".cls-1").css("opacity", 0);
+            $(".cls-2").css("opacity", 0);
+            $(".cls-4").css("opacity", 0);
+            $(".cls-5").css("opacity", 0);
+            
+        }, function(){
+            $(this).css("color", "rgba(0,0,0,.54)");
+            $(".cls-1").css("opacity", 1);
+            $(".cls-2").css("opacity", 1);
+            $(".cls-4").css("opacity", 1);
+            $(".cls-5").css("opacity", 1);
+        })
+
+        $("#zone5").hover(function(){
+            $(this).css("color", "rgba(0,0,0,1)");
+            $(".cls-3").css("opacity", 0);
+            $(".cls-2").css("opacity", 0);
+            $(".cls-4").css("opacity", 0);
+            $(".cls-5").css("opacity", 0);
+        }, function(){
+            $(this).css("color", "rgba(0,0,0,.54)");
+            $(".cls-3").css("opacity", 1);
+            $(".cls-2").css("opacity", 1);
+            $(".cls-4").css("opacity", 1);
+            $(".cls-5").css("opacity", 1);
+        })
+
+        $("#zone4").hover(function(){
+            $(this).css("color", "rgba(0,0,0,1)");
+            $(".cls-1").css("opacity", 0);
+            $(".cls-3").css("opacity", 0);
+            $(".cls-4").css("opacity", 0);
+            $(".cls-5").css("opacity", 0);
+        }, function(){
+            $(this).css("color", "rgba(0,0,0,.54)");
+            $(".cls-1").css("opacity", 1);
+            $(".cls-3").css("opacity", 1);
+            $(".cls-4").css("opacity", 1);
+            $(".cls-5").css("opacity", 1);
+        })
+        $("#zone1").hover(function(){
+            $(this).css("color", "rgba(0,0,0,1)");
+            $(".cls-1").css("opacity", 0);
+            $(".cls-2").css("opacity", 0);
+            $(".cls-3").css("opacity", 0);
+            $(".cls-5").css("opacity", 0);
+        }, function(){
+            $(this).css("color", "rgba(0,0,0,.54)");
+            $(".cls-1").css("opacity", 1);
+            $(".cls-2").css("opacity", 1);
+            $(".cls-3").css("opacity", 1);
+            $(".cls-5").css("opacity", 1);
+        })
+        $("#zone3").hover(function(){
+            $(this).css("color", "rgba(0,0,0,1)");
+            $(".cls-1").css("opacity", 0);
+            $(".cls-2").css("opacity", 0);
+            $(".cls-4").css("opacity", 0);
+            $(".cls-3").css("opacity", 0);
+        }, function(){
+            $(this).css("color", "rgba(0,0,0,.54)");
+            $(".cls-1").css("opacity", 1);
+            $(".cls-2").css("opacity", 1);
+            $(".cls-4").css("opacity", 1);
+            $(".cls-3").css("opacity", 1);
+        })
